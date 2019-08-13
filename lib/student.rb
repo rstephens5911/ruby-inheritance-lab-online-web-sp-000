7e1146a5e42df9f9b5c9fb08d294c105588a436c
Ronald Stephens <rstephens@bluemorpho.cc>
2019-08-13 19:23:09 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require_relative "./user.rb"
+
+class Student < User
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn (something_to_learn)
+    @knowledge << something_to_learn
+  end
+
+  def knowledge
+    @knowledge
+  end
+
 
 end
